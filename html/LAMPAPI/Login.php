21f488562340f14a88f82822e933f371b578431b
Matthew Daley <122408204+matt0125@users.noreply.github.com>
2023-08-29 18:44:55 -0400

    Update login API
    Returns new error "Incorrect password" if the user exists but the password is incorrect.

--- html/LAMPAPI/Login.php
+++ html/LAMPAPI/Login.php
@@ -14,14 +14,21 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+		$stmt = $conn->prepare("SELECT ID,firstName,lastName,password FROM Users WHERE Login=?");
+		$stmt->bind_param("s", $inData["login"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
 		if( $row = $result->fetch_assoc()  )
 		{
-			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
+			if( strcmp($row['password'], $inData["password"]) === 0 )
+			{
+				returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
+			}
+			else
+			{
+				returnWithError("Incorrect password");
+			}
 		}
 		else
 		{
