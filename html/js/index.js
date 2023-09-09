37c8c8392f1c1305609a34594ba82b3f8332f64f
Evan Naderi <evan.naderi@gmail.com>
2023-09-09 15:52:06 -0400

    added login API call

--- html/js/index.js
+++ html/js/index.js
@@ -20,3 +20,59 @@ function toggleForm(formToShow) {
         loginButton.classList.remove("btn-primary");
     }
 }
+
+async function login(username, password) {
+    try {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            var raw = JSON.stringify({
+            "login": username,
+            "password": password
+            });
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+            
+        const response = await fetch('http://poosd-project.com/LAMPAPI/Login.php', requestOptions);
+
+        const data = await response.json();
+        if (data.id !== 0) {
+            return {
+                success: true,
+                userId: data.id,
+                firstName: data.firstName,
+                lastName: data.lastName
+            };
+        } else {
+            return {
+                success: false,
+                error: data.error
+            };
+        }
+    } catch (error) {
+        console.error("There was an issue with the fetch operation:", error.message);
+        return {
+            success: false,
+            error: 'Network or server error'
+        };
+    }
+}
+
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+
+    const result = await login(username, password);
+    if (result.success) {
+        console.log('Successfully logged in with token:', result.token);
+    } else {
+        console.error('Login failed:', result.error);
+    }
+});
